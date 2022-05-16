41812fd3551d65d2028b4a5250b1415b5d4e372a
Riccardo <riccardo.bellini@stud.unifi.it>
2022-05-16 16:23:47 +0200

    added class Vehicle

--- Vehicle.java
+++ Vehicle.java
@@ -1,5 +1,37 @@
+import java.io.*;
+
+public enum VehicleState{
+    FREE,
+    OCCUPED,
+    MAINTENANCE,
+    DAMAGED;
+}
+
 
 public class Vehicle {
+    public int id;
+    public String type;
+    public VehicleState state;
+    public int batteryLeft;
+    public List damage = new ArrayList();
+    public String lastUse;
+
+
+    public Vehicle(int id, String type, VehicleState state){
+        this.id = id;
+        this.type = type;
+        this.state = state;
+    }
+
+    public boolean updateState(VehicleState newState){
+        this.state = newState;
+        System.out.println("State modified in: "+newState);
+        return true;
+    }
+
+
+
+
 
 
 
