6dce0c7bdfe279ef5e5cbd4ae5f9623a6c9e47eb
melizo4ka <64137900+melizo4ka@users.noreply.github.com>
2022-05-16 12:50:56 +0200

    Added classes client and manager

---
+++ Client.java
@@ -0,0 +1,60 @@
+public class Client {
+    //variables
+    int id;
+    String password;
+    int money;
+    List achievements = new ArrayList();
+    int vehicleOccupied;
+
+    //constructor
+    public Client(int id, String password, int money){
+        this.id = id,
+        this.password = password;
+        this.money = money;
+        this.vehicleOccupied = 0;
+    }
+
+    //methods
+    public boolean changePw(String password){
+        if(password.contains(".")) {
+            this.password = password;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public int addMoney(int moneyAdded){
+        this.money = money + moneyAdded;
+        return money;
+    }
+
+    public List getAchievements() {
+        return achievements;
+    }
+
+    public boolean getVehicle(int vehicleID){
+        if (vehicleID.state == FREE && this.vehicleOccupied == 0){
+            vehicleID == OCCUPIED; // non sono sicura chi fa l'assegnamento dello stato del veicolo
+            this.vehicleOccupied = vehicleID;
+            return true;
+        }
+        else {
+            print("Vehicle is occupied or You are already renting a vehicle!")
+            return false;
+        }
+    }
+
+    public void leaveVehicle(){
+        vehicleID == FREE;
+        this.vehicleOccupied = 0;
+        print("Do you want to note a problem?")
+        //get input from user
+        //if (wants to input)
+        //  noteProblem()
+    }
+
+    public void noteProblem(){
+        //some code as a list of problems
+    }
+}
---
+++ Manager.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+
+public class Manager {
+
+    //veicoli in una mappa?
+    HashMap<Integer, String> vehicles = new HashMap<Integer, String>();
+
+    //clienti in una lista?
+    List clients = new ArrayList();
+
+    //static variable reference of single_instance of type Singleton
+    private static Singleton single_instance = null;
+
+    //private constructor
+    private Singleton(){
+    }
+
+    //method to create instance of Singleton class
+    public static Singleton getInstance()
+    {
+        if (single_instance == null)
+            single_instance = new Singleton();
+        return single_instance;
+    }
+
+    public void addVehicle(String numberPlate){
+        vehiclesAmount = vehicles.size();
+        vehicles.put(vehiclesAmount + 1, numberPlate);
+    }
+
+    public void removeVehicle(Integer number){
+        vehicles.remove(number);
+    }
+
+    public int addClient(int id){
+        clientsAmount = len(clients);
+        clients.add(cliensAmount + 1, id);
+        return id;
+    }
+
+    public void removeClient(int index){
+        clients.remove(index);
+    }
+}
